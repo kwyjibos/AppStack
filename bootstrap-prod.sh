c478aa6faea0d45c86c0b27f93724ab87609d0bc
Daniel Spain <daniel.christopher.spain@gmail.com>
2017-12-05 22:22:09 +0000

    tweaks for prod bootstrap

---
+++ bootstrap-prod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update \
+&& apt-get install -y git docker.io python-pip \
+&& pip install docker-compose \
+&& git clone https://github.com/kwyjibos/AppStack.git /opt/AppStack \
+&& cd /opt/Appstack \
+&& docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
